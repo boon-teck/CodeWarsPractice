59c382db9d6227522ba4b73b55d00e7761ac7926
Boon <boonteck.soh@hotmail.com>
2023-03-18 22:18:04 +0800

    Added Square(n) Sum practice

---
+++ 8kyu/Square(n) Sum.js
@@ -0,0 +1,17 @@
+function squareSum(numbers){ 
+    //es6 reduce
+    if (numbers.length > 0) {
+      return numbers.reduce((acc,num)=> acc + num * num , 0)
+    }
+    else return 0
+    
+    //normal loop
+    if (numbers.length>0){
+      let total = 0
+      for (i=0; i<numbers.length; i++){
+        total += Math.pow(numbers[i],2)
+      }
+      return total
+    }
+    else return 0
+  }
