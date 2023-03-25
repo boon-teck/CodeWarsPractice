8fca33579e5b2eb48d15a5330fa4f367c0878bd2
Boon <boonteck.soh@hotmail.com>
2023-03-26 00:20:20 +0800

    modified FindMultiplesOfANumber

--- 8kyu/FindMultiplesOfANumber.js
+++ 8kyu/FindMultiplesOfANumber.js
@@ -8,3 +8,18 @@ function findMultiples(integer, limit) {
     }
     return arrayOfNumbers
 }
+
+//+integer step within for loop
+function findMultiples(integer, limit) {
+    //your code here
+    let arrayOfNumbers = []
+      for (let i=integer; i <= limit; i+=integer){
+      arrayOfNumbers.push(i)
+      }
+      return arrayOfNumbers
+  }
+  
+//using array constructor and fill method and map method. 
+function findMultiples3(integer, limit){
+  return new Array(Math.floor(limit/integer)).fill(1).map( (num, index)=> integer * (index+1)) //Index+1 as index starts from 0.
+}
