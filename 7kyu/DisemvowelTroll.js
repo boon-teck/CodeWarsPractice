262d38abcda08d1eb44542407b4159902a873179
Boon <boonteck.soh@hotmail.com>
2023-03-29 23:40:39 +0800

    added DismvowelTroll.js

---
+++ 7kyu/DisemvowelTroll.js
@@ -0,0 +1,15 @@
+function disemvowel(str) {
+    let returnedStrArray = []
+    let strArray = str.split("")
+    for (i=0; i < strArray.length; i++){
+      if(!'aeiouAEIOU'.includes(strArray[i])) {
+        returnedStrArray.push(strArray[i])
+      }
+    }
+    return returnedStrArray.join("")
+}
+
+//filter method
+function disemvowel(str) {
+    return str.split("").filter(str => !'aeiouAEIOU'.includes(str)).join("")
+}
