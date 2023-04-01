58d740c27b9a0a43161e0fad730a463c472750dd
Boon <boonteck.soh@hotmail.com>
2023-04-01 18:18:48 +0800

    Added HighestAndLowest

---
+++ 7kyu/HighestAndLowest.js
@@ -0,0 +1,21 @@
+function highAndLow(numbers){
+    let numbersArray = numbers.split(" ");
+    let highest, lowest;
+    for (let i=0; i<numbersArray.length; i++){
+      if (!highest || !lowest) {
+        highest = Number(numbersArray[i])
+        lowest = Number(numbersArray[i])
+      } else if (numbersArray[i]>highest) {
+        highest = Number(numbersArray[i])
+      } else if (numbersArray[i]<lowest) {
+        lowest = Number(numbersArray[i])
+      }
+    }
+    return `${highest} ${lowest}`
+}
+
+//using array sort method
+function highAndLow(numbers){
+    orderedArr = numbers.split(" ").sort((a,b) => a-b)
+    return `${orderedArr[orderedArr.length-1]} ${orderedArr[0]}`
+}
