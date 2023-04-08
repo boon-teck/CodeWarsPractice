0dfee9b93e2434d45be008465aed9737addf771c
Boon <boonteck.soh@hotmail.com>
2023-04-08 23:32:51 +0800

    Added TwoOldestAges.js

---
+++ 7kyu/TwoOldestAges.js
@@ -0,0 +1,21 @@
+/* 
+The two oldest ages function/method needs to be completed. It should take an array of numbers as its argument and return the two highest numbers within the array. The returned value should be an array in the format [second oldest age,  oldest age].
+
+The order of the numbers passed in could be any order. The array will always include at least 2 items. If there are two or more oldest age, then return both of them in array format.
+*/
+
+// return the two oldest/oldest ages within the array of ages passed in.
+function twoOldestAges(ages){
+    let largestAge = Math.max(...ages)
+    let twoOldestAges = []
+    for (let i = 0; i < ages.length; i++){
+      if (ages[i] == largestAge){
+        ages.splice(i,1)
+        let secondLargestAge = Math.max(...ages)
+        twoOldestAges = [secondLargestAge,largestAge]
+        break
+      }
+    }
+    return twoOldestAges
+}
+  
